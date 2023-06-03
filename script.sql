7cadbb6fe2914510d2ed0c1c53e921081c9450a0
Micael <micaelrrocha13@gmail.com>
2023-06-03 16:38:35 -0300

    by Micael

---
+++ script.sql
@@ -0,0 +1,198 @@
+-- Brena dos Santos Freitas / RA: 2465710
+-- Maria Gabriella Victor Gomes da Silva / RA: 2143364
+-- Micael Ribeiro Rocha / RA: 2454424
+
+
+-- 2.Implementação no PostGreSQL (3,5 pontos):
+-- Geração e execução do código SQL para criação das tabelas e inserção de pelo menos 5 linhas de dados por tabela (0,5):
+CREATE TABLE servico(
+	id_servico SERIAL,
+	tipo varchar(100),
+	valor numeric(9, 2),
+	primary key(id_servico)
+);
+CREATE TABLE funcionario(
+	id_funcionario SERIAL,
+	id_servico int, 
+	idade INTEGER,
+	nome VARCHAR(100),
+	cpf INTEGER,
+	rg INTEGER,
+	funcao CHAR(100),
+	primary key(id_funcionario),
+	FOREIGN KEY (id_servico) REFERENCES servico (id_servico)
+);
+
+CREATE TABLE produto(
+	id_produto SERIAL,
+	nome_produto varchar(100),
+	valor numeric(9, 2),
+    quantidade int,
+	primary key(id_produto)
+)
+CREATE TYPE tipo_motor AS ENUM('E', 'C', 'H');
+CREATE TABLE veiculo (
+	 id_veiculo SERIAL,
+	 ano 		INTEGER,
+     placa     	VARCHAR(100),
+     marca		VARCHAR(100),
+     modelo    	VARCHAR(100),
+     cor      	VARCHAR(100),
+	 motor		tipo_motor,
+	primary key (id_veiculo)
+);
+CREATE TABLE cliente(
+	id_cliente SERIAL,
+	nome_cliente varchar(100),
+	idade INTEGER,
+	id_servico INTEGER,
+	id_produto integer,
+	id_veiculo integer,
+	cpf INTEGER,
+	rg INTEGER,
+	telefone varchar(100),
+	data_de_registro TIMESTAMP,
+	primary key(id_cliente),
+	FOREIGN KEY (id_servico) REFERENCES servico (id_servico),
+	FOREIGN KEY (id_produto) REFERENCES produto (id_produto),
+	FOREIGN KEY (id_veiculo) REFERENCES veiculo (id_veiculo));
+	
+insert into servico (tipo, valor)
+values ('Troca de óleo', 200.00);
+
+insert into servico (tipo, valor)
+values ('Revisão detalhada do sistema de arrefecimento', 120.00);
+
+insert into servico (tipo, valor)
+values ('Troca do filtro do ar', 230.00);
+
+insert into servico (tipo, valor)
+values ('Manutenção de embreagem', 300.00);
+
+insert into servico (tipo, valor)
+values ('Revisão dos componentes do freio', 300.00);
+
+insert into veiculo (ano,placa, marca, modelo, cor, motor) values (2021, 'SDC-1584', 'Honda', 'Civic', 'preto','H');
+insert into veiculo (ano, placa, marca, modelo, cor, motor) values (2010, 'XCR-2165', 'Volkswagen', 'Gol','Vermelho','C');
+insert into veiculo (ano, placa, marca, modelo, cor, motor) values (2020, 'RHY-3265', 'Ford', 'Fusion','Cinza','H');
+insert into veiculo (ano, placa, marca, modelo, cor, motor) values (2022, 'KLC-0376', 'Tesla','Branco', 'ModelS','E');
+insert into veiculo (ano, placa, marca, modelo, cor, motor) values (1994, 'LIU-9875', 'Volkswagen', 'Fusca','Verde','C');
+
+insert into produto (nome_produto,valor,quantidade) 
+values ('Óleo de carro semissintétic0', 30.00, 10);
+
+insert into produto (nome_produto,valor,quantidade) 
+values ('Roda  Aro 15 4 x100', 590.50, 10);
+
+insert into produto (nome_produto,valor,quantidade) 
+values ('Motor', 2439.95, 6);
+
+insert into produto (nome_produto,valor,quantidade) 
+values ('Disco de freio dianteiro', 381, 12);
+
+insert into produto (nome_produto,valor,quantidade) 
+values ('Lubrificante 0W20 honda sintético', 90.25, 15);
+
+insert into cliente (nome_cliente, idade, id_servico, id_produto, id_veiculo, cpf , rg , telefone , data_de_registro)
+values ('Josue do Santos da Silva', 30, 1, 2, 3, 098098098, 32424242, 73999988888, CURRENT_TIMESTAMP);
+
+insert into cliente (nome_cliente, idade, id_servico, id_produto, id_veiculo, cpf , rg , telefone , data_de_registro)
+values ('Mariana Gomes Rocha', 22, 3, 2, 1, 54454545, 343242, 21777774444, CURRENT_TIMESTAMP);
+
+insert into cliente (nome_cliente, idade, id_servico, id_veiculo, cpf , rg , telefone , data_de_registro)
+values ('Micael Ribeiro Rocha', 19, 1, 5, 321123213, 123213323, 5433339999, CURRENT_TIMESTAMP);
+
+insert into funcionario (id_servico, idade, nome, cpf, rg, funcao)
+values ('3', 42, 'Jorse Santos Pratos', 234242423, 34555, 'Mecanico');
+
+insert into funcionario (id_servico, idade, nome, cpf, rg, funcao)
+values ('3', 26, 'Jessica Passos Braga', 234242423, 34555, 'Mecanico');
+
+-- Criação de trigger/função de backup de dados excluídos de uma tabela, registrando os dados da linha apagada (sem restrições de chave), com usuário e data (0,5):
+
+CREATE TABLE backup_cliente(
+	id_cliente SERIAL,
+	nome_cliente varchar(100),
+	idade INTEGER,
+	id_servico INTEGER,
+	id_produto integer,
+	id_veiculo integer,
+	cpf INTEGER,
+	rg INTEGER,
+	telefone varchar(100),
+	data_de_registro TIMESTAMP,
+	primary key(id_cliente));
+
+CREATE OR REPLACE FUNCTION backup()
+RETURNS TRIGGER
+AS
+$$
+BEGIN
+	INSERT INTO backup_cliente(nome_cliente, idade, id_servico, id_produto, id_veiculo, cpf, rg, telefone, data_de_registro)
+	VALUES(OLD.nome_cliente, OLD.idade, OLD.id_servico, OLD.id_produto, OLD.id_veiculo, OLD.cpf, OLD.rg, OLD.telefone, OLD.data_de_registro);
+	RETURN OLD;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER backup AFTER DELETE ON cliente
+FOR EACH ROW EXECUTE PROCEDURE backup();
+
+DELETE FROM cliente WHERE id_cliente = 1;
+
+-- Criação de pelo menos 4 índices (1 índice por tabela no máximo) (0,5):
+CREATE INDEX indice_veiculo ON veiculo(id_veiculo);
+CREATE INDEX indice_produto ON produto(id_produto);
+CREATE INDEX indice_servico ON servico(id_servico);
+CREATE INDEX indice_cliente ON cliente(id_cliente);
+CREATE INDEX indice_funcionario ON funcionario(id_funcionario);
+
+-- Criação de usuário e atribuição de privilégio somente de leitura das tabelas para o usuário criado (0,5):
+
+CREATE ROLE usuario WITH
+LOGIN
+PASSWORD 'usuario';
+
+-- Criação de no mínimo uma visão (view) a partir de duas ou mais tabelas (0,5):
+CREATE VIEW carro_cliente as 
+SELECT nome_cliente, marca, modelo FROM cliente cli, veiculo veic
+WHERE cli.id_veiculo = veic.id_veiculo;
+
+-- Criação de trigger que permita inserção de dados na view, e atribuição de privilégio ao usuário criado anteriormente para ver e inserir usando a view (1,0):
+CREATE OR REPLACE FUNCTION carro_cliente() RETURNS TRIGGER
+AS $$
+BEGIN
+	insert into veiculo (marca, modelo) 
+	values (NEW.marca, NEW.modelo);
+	insert into cliente (nome_cliente, id_veiculo, data_de_registro)
+	values (NEW.nome_cliente, 3, CURRENT_TIMESTAMP);						
+	RETURN NEW;							
+END;
+$$ LANGUAGE plpgsql;
+					  
+CREATE TRIGGER carro_cliente INSTEAD OF
+INSERT ON carro_cliente FOR EACH ROW EXECUTE
+PROCEDURE carro_cliente();
+
+GRANT SELECT, INSERT ON carro_cliente TO
+usuario;
+
+insert into carro_cliente(nome_cliente, marca, modelo)
+values('Zeze dos Santos Jesus', 'Nissa', 'Sylvia');	
+
+
+
+
+
+select * from servico;
+select * from veiculo; 
+select * from produto;
+select * from cliente;
+select * from funcionario;
+select * from backup_cliente;
+select * from carro_cliente;
+
+
+
+
+
