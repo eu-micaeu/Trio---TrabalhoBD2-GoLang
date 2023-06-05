4279bf9de72c4bed890d7b6151521034d5b5ec0d
Micael <micaelrrocha13@gmail.com>
2023-06-05 06:04:05 -0300

    by Micael

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,11 @@ func Servidor() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./index.html")
 	})
+
+	http.HandleFunc("/menu", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/menu.html", http.StatusSeeOther)
+	})
+
 	port := ":8081"
 	fmt.Printf("\nServidor rodando em http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
